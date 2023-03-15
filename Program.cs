da92125513f95062b77dafba4e0aabba0c80ae74
Pip Matthews <pip.matthews+github@gmail.com>
2023-03-14 23:58:04 -0400

    testing all poss outcomes

--- Program.cs
+++ Program.cs
@@ -90,105 +90,175 @@ namespace BlackJackCS
 
         static void PlayerWins()
         {
+            Console.WriteLine();
             Console.WriteLine("Yay, you won!");
+            Console.WriteLine();
         }
+
+        static void DealerWins()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Bad luck! The Dealer won this round. ");
+            Console.WriteLine();
+        }
+
 
         static void Main(string[] args)
         {
-            //creating the deck
-            Console.WriteLine("Welcome to C#");
-            var suits = new List<string>() { "Hearts", "Diamonds", "Clubs", "Spades" };
-            var faces = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
-            var deck = new List<string>() { };
+            string playAgain = "YES";
 
-            foreach (var suit in suits)
+            while (playAgain == "YES")
             {
-                foreach (var face in faces)
+                //creating the deck
+                var suits = new List<string>() { "Hearts", "Diamonds", "Clubs", "Spades" };
+                var faces = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+                var deck = new List<string>() { };
+
+                foreach (var suit in suits)
                 {
-                    var card = $"{face} of {suit}";
-                    deck.Add(card);
+                    foreach (var face in faces)
+                    {
+                        var card = $"{face} of {suit}";
+                        deck.Add(card);
+                    }
                 }
-            }
 
-            //shuffle the cards using youtube method
-            var decreasingIndex = deck.Count - 1; //sets the var as the last element
-            //creates a random number and then subtracts 1 as the index number goes from 0 to length-1
+                //shuffle the cards using youtube method
+                var decreasingIndex = deck.Count - 1; //sets the var as the last element
+                                                      //creates a random number and then subtracts 1 as the index number goes from 0 to length-1
 
-            while (decreasingIndex >= 0)
-            {
-                Random r = new Random();
-                var randomIndex = r.Next(0, deck.Count);
-
-                var placeHolder1 = deck[decreasingIndex];
-                var placeHolder2 = deck[randomIndex];
-
-                deck[decreasingIndex] = placeHolder2;
-                deck[randomIndex] = placeHolder1;
-
-                //if we aren't at the first card, then look at the next one down the line and continue the shuffle
-                decreasingIndex = decreasingIndex - 1;
-
-
-            }
-
-            int counter = 0;
-            while (counter < 52)
-            {
-                Console.WriteLine(deck[counter]);
-                counter++;
-            }
-
-            //populate the strings for player and dealer's hands
-            int cardsDealt = 0;
-            var dealersHand = new List<string>() { deck[cardsDealt] };
-            cardsDealt++;
-            dealersHand.Add(deck[cardsDealt]);
-            cardsDealt++;
-            var playersHand = new List<string>() { deck[cardsDealt] };
-            cardsDealt++;
-            playersHand.Add(deck[cardsDealt]);
-            cardsDealt++;
-
-            //calculate the totals
-            int dealersTotal = CountTotal(dealersHand);
-            int playersTotal = CountTotal(playersHand);
-            Console.WriteLine($"the dealer's total is {dealersTotal}");
-            Console.WriteLine($"the player's total is {playersTotal}");
-
-            PrintPlayersHand(playersHand);
-            PrintDealersHand(dealersHand);
-
-
-
-            while (playersTotal < 21)
-            {
-                Console.Write($"Would you like to HIT or STAND? ");
-                string decision = "";
-                decision = Console.ReadLine();
-
-                if (decision == "HIT")
+                while (decreasingIndex >= 0)
                 {
-                    playersHand.Add(deck[cardsDealt]);
+                    Random r = new Random();
+                    var randomIndex = r.Next(0, deck.Count);
+
+                    var placeHolder1 = deck[decreasingIndex];
+                    var placeHolder2 = deck[randomIndex];
+
+                    deck[decreasingIndex] = placeHolder2;
+                    deck[randomIndex] = placeHolder1;
+
+                    //if we aren't at the first card, then look at the next one down the line and continue the shuffle
+                    decreasingIndex = decreasingIndex - 1;
+
+
+                }
+
+                //int counter = 0;
+                //while (counter < 52)
+                //{
+                //    Console.WriteLine(deck[counter]);
+                //    counter++;
+                //}
+
+                //populate the strings for player and dealer's hands
+                int cardsDealt = 0;
+                var dealersHand = new List<string>() { deck[cardsDealt] };
+                cardsDealt++;
+                dealersHand.Add(deck[cardsDealt]);
+                cardsDealt++;
+                var playersHand = new List<string>() { deck[cardsDealt] };
+                cardsDealt++;
+                playersHand.Add(deck[cardsDealt]);
+                cardsDealt++;
+
+                //calculate the totals
+                int dealersTotal = CountTotal(dealersHand);
+                int playersTotal = CountTotal(playersHand);
+                //Console.WriteLine($"the dealer's total is {dealersTotal}");
+                //Console.WriteLine($"the player's total is {playersTotal}");
+
+                PrintPlayersHand(playersHand);
+                //PrintDealersHand(dealersHand);
+
+
+
+                while (playersTotal < 21)
+                {
+                    Console.Write($"Would you like to HIT or STAND? ");
+                    string decision = "";
+                    decision = Console.ReadLine();
+
+                    if (decision == "HIT")
+                    {
+                        playersHand.Add(deck[cardsDealt]);
+                        cardsDealt++;
+                        playersTotal = CountTotal(playersHand);
+                        PrintPlayersHand(playersHand);
+                        //Console.WriteLine($"the player's total is {playersTotal}");
+                    }
+                    else if (decision == "STAND")
+                    {
+                        Console.WriteLine("You have chosen to stick with your cards. Now the dealer will take a turn.");
+                        PrintDealersHand(dealersHand);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter either HIT or STAND");
+                    }
+                }
+
+                while (dealersTotal < 17)
+                {
+                    Console.WriteLine("The Dealer has chosen to HIT");
+                    dealersHand.Add(deck[cardsDealt]);
                     cardsDealt++;
-                    playersTotal = CountTotal(playersHand);
-                    PrintPlayersHand(playersHand);
-                    Console.WriteLine($"the player's total is {playersTotal}");
+                    dealersTotal = CountTotal(dealersHand);
+                    PrintDealersHand(dealersHand);
+                    Console.WriteLine($"the dealer's total is {dealersTotal}");
                 }
-                if (decision == "STAND")
+
+                if (dealersTotal == 17)
                 {
-                    Console.Write("You have chosen to stick with your cards. Now the dealer will take a turn.");
-                    break;
+                    //PrintDealersHand(dealersHand);
+                    Console.WriteLine("The dealer has chosen to STAND");
+                    if (dealersTotal >= playersTotal)
+                    {
+                        Console.WriteLine($"the dealer's total is {dealersTotal}");
+                        Console.WriteLine($"the player's total is {playersTotal}");
+                        DealerWins();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"the dealer's total is {dealersTotal}");
+                        Console.WriteLine($"the player's total is {playersTotal}");
+                        PlayerWins();
+                    }
                 }
+
+                if (dealersTotal > 17 && dealersTotal <= 21)
+                {
+                    //PrintDealersHand(dealersHand);
+                    Console.WriteLine("The dealer has chosen to STAND");
+                    if (dealersTotal >= playersTotal)
+                    {
+                        Console.WriteLine($"the dealer's total is {dealersTotal}");
+                        Console.WriteLine($"the player's total is {playersTotal}");
+                        DealerWins();
+                    }
+                }
+
+                if (dealersTotal > 21)
+                {
+                    //PrintDealersHand(dealersHand);
+                    //Console.WriteLine($"the dealer's total is {dealersTotal}");
+                    //Console.WriteLine($"the player's total is {playersTotal}");
+                    Console.WriteLine("The dealer has gone bust! ");
+                    PlayerWins();
+                }
+
+
+                if (playersTotal == 21 && dealersTotal != 21)
+                {
+                    Console.WriteLine($"the dealer's total is {dealersTotal}");
+                    Console.WriteLine($"the player's total is {playersTotal}");
+                    PlayerWins();
+                }
+
+                Console.WriteLine("Would you like to play again? Choose YES or NO ");
+                playAgain = Console.ReadLine();
             }
-
-
-
-            if (playersTotal == 21)
-            {
-                PlayerWins();
-            }
-
-
 
 
         }
